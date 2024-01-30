57c2dcc406e6173e9918ad53c404914dcb753017
PhilipSamuelsson <112427706+PhilipSamuelsson@users.noreply.github.com>
2024-01-30 23:53:53 +0100

    Add font styles and navbar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="navbar flex items-center justify-between flex-row bg-gray-400 p-5  ">
+          <div className="logo-container">
+            <a href="/"className="logo font-poppins font-semibold text-2xl">The Barking Lot</a>
+            </div>
+            <div className="links-container flex gap-5">
+              <a href="/about" className="link font-poppins font-semibold">About</a>
+              <a href="/contact"className="link font-poppins font-semibold">Contact</a>
+              <a href="/dogs" className="link font-poppins font-semibold">Dogs</a>
+           
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
