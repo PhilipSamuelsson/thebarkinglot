fae98d062b64818eb09e83f0f461af5d0bc1efe9
PhilipSamuelsson <112427706+PhilipSamuelsson@users.noreply.github.com>
2024-01-30 23:55:50 +0100

    123

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="navbar flex items-center justify-between flex-row bg-gray-400 p-5  ">
           <div className="logo-container">
-            <a href="/"className="logo font-poppins font-semibold text-2xl">The Barking Lot</a>
+            <a href="/"className="logo font-poppins font-semibold text-2xl">The Barking Lot!</a>
             </div>
             <div className="links-container flex gap-5">
               <a href="/about" className="link font-poppins font-semibold">About</a>
