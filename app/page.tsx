88446a81d96d3129a6d188109c87e653f8abf3a8
PhilipSamuelsson <philip.samuelsson@iths.se>
2024-01-31 23:38:04 +0100

    Add background image to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
+    return (
+        <>
+            <section>
+                <div className="relative w-full h-screen">
+                    {/* Image Container */}
+                    <div className="relative w-full h-full">
+                        <Image
+                            src={
+                                'https://images.unsplash.com/photo-1518882174711-1de40238921b?q=80&w=2458&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' as string
+                            }
+                            layout="fill"
+                            objectFit="cover"
+                            quality={100}
+                            alt="Background"
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
